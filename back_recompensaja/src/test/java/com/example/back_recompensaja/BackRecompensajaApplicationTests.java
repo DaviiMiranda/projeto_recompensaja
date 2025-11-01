7f0a41ba3123299dd02ec91bc7aaa0b369817a2a
Davi <davimiranda210@gmail.com>
2025-11-01 17:17:16 -0300

    Adiciona front-end ao projeto principal

---
+++ back_recompensaja/src/test/java/com/example/back_recompensaja/BackRecompensajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_recompensaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRecompensajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
